7855b44070d91d196fb05bf892557e0976129841
Diego Napoli <d.napoli.dev@gmail.com>
2020-08-15 22:16:19 -0300

    Pipefy React JS
    First Commit

---
+++ src/components/Board/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import produce from 'immer';
+import { loadLists } from '../../Services/api';
+
+import List from '../List';
+import BoardContext from './context';
+import { Container } from './styles';
+
+const data = loadLists();
+
+function Board() {
+  const [lists, setLists] = useState(data);
+
+  function move(fromList, toList, from, to) {
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }));
+  }
+
+  return (
+      <BoardContext.Provider value={{ lists, move }}>
+        <Container>
+          {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}
+        </Container>
+      </BoardContext.Provider>
+  );
+}
+
+export default Board;
